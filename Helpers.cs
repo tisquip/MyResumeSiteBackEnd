46b4c6ac67a7f0ee61e7a6e3636c72c6d9b07dbd
Software Architects of Southern Africa (SASA) <soarsoaf@outlook.com>
2021-05-11 15:24:12 +0200

    Apikeys

---
+++ Helpers.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace MyResumeSiteBackEnd
+{
+    public static class Helpers
+    {
+        public static (string ApiKey, int TeamId, int LeagueId) GetApiVariable(IConfiguration configuration)
+        {
+            (string ApiKey, int TeamId, int LeagueId) vtr = ("", 0, 0);
+            vtr.ApiKey = configuration.GetValue<string>("ApiFootballKey");
+            vtr.TeamId = configuration.GetValue<int>("TeamId");
+            vtr.LeagueId = configuration.GetValue<int>("LeagueId");
+
+            return vtr;
+        }
+    }
+}
