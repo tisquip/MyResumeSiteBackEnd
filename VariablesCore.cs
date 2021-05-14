d4a9d3f5b364c296b953d44de827e42066af9039
Software Architects of Southern Africa (SASA) <soarsoaf@outlook.com>
2021-05-15 01:27:58 +0200

    Moved back core

---
+++ VariablesCore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyResumeSiteModels
+{
+    public static class VariablesCore
+    {
+        public static string MessageHubUrlEndPointWithPreSlash => "/messages";
+        public static string SignalRMethodNameFixturesUpdated => "RealTimeFixturesChanged";
+        public static string SignalRMethodNameLiveMatch => "RealTimeLiveMatch";
+        public static string SignalRMethodNameStandings => "RealTimeStandings";
+        public static string ServerUrl => "https://myresumesitebackend.azurewebsites.net"; //"https://localhost:44371/";
+
+        public static string SignalRFullEndpoint()
+        {
+            string messageEndPoint = MessageHubUrlEndPointWithPreSlash;
+            messageEndPoint = messageEndPoint.Remove(0, 1);
+            return ServerUrl + messageEndPoint;
+        }
+    }
+}
